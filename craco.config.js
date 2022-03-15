67d8d145da4745c6ffd318ea91ed1f07e4e1e3b4
Hariom Sharma <hariom.s2510@gmail.com>
2022-03-15 23:48:09 +0530

    fix: build system

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,15 @@
-const CracoEsbuildPlugin = require('craco-esbuild')
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
-const webpack = require('webpack')
+
+// const webpack = require('webpack')
+// const CracoEsbuildPlugin = require('craco-esbuild')
 
 module.exports = {
-  plugins: [{ plugin: CracoEsbuildPlugin }],
+  // plugins: [{ plugin: CracoEsbuildPlugin }],
   webpack: {
     plugins: [
-      new webpack.ProvidePlugin({
-        React: 'react'
-      }),
+      // new webpack.ProvidePlugin({
+      //   React: 'react'
+      // }),
       new SimpleProgressWebpackPlugin()
     ]
   },
