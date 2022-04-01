ba28631531c25046292894499865876ab1fc4d34
Hariom Sharma <hariom.s2510@gmail.com>
2022-04-01 19:21:08 +0530

    feat: download users from backend

--- src/api/mutations/useExportOrganizationUsersMutation.ts
+++ src/api/mutations/useExportOrganizationUsersMutation.ts
@@ -4,14 +4,13 @@ import { useSnackbar } from 'notistack'
 import { useMutation } from 'react-query'
 import { UseMutationOptions } from 'react-query/types/react/types'
 import axiosGrowthDay from '../../axios/axiosGrowthDay'
-import { PagedResultOrganizationUser } from '../../types/api'
-import { jsonToXlsxFile } from '../../utils/sheetsUtil'
+import { OrganizationUsersRequest } from '../queries/useOrganizationUsersQuery'
 
 export const EXPORT_ORGANIZATION_USERS_MUTATION_KEY = ['GROWTHDAY', 'MUTATION', 'EXPORT_ORGANIZATION_USERS']
 
 const useExportOrganizationUsersMutation = (
   options: Omit<
-    UseMutationOptions<PagedResultOrganizationUser, unknown, void, typeof EXPORT_ORGANIZATION_USERS_MUTATION_KEY>,
+    UseMutationOptions<Blob, unknown, void, typeof EXPORT_ORGANIZATION_USERS_MUTATION_KEY>,
     'queryKey' | 'queryFn'
   > = {}
 ) => {
@@ -19,10 +18,13 @@ const useExportOrganizationUsersMutation = (
   return useMutation(
     EXPORT_ORGANIZATION_USERS_MUTATION_KEY,
     () =>
-      // axiosGrowthDay.get<PagedResultOrganizationUser>('/organizationUsers/download', {
-      //   params: { offset: 0, limit: 0 }
-      // }),
-      axiosGrowthDay.get<PagedResultOrganizationUser>('/organizationUsers', { params: { offset: 0, limit: 0 } }),
+      axiosGrowthDay.get<Blob>('/organizationUsers/download', {
+        params: { offset: 0, limit: 0, invitationPending: false } as OrganizationUsersRequest,
+        responseType: 'blob',
+        headers: {
+          Accept: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        }
+      }),
     {
       ...options,
       onMutate: (...rest) => {
@@ -30,10 +32,7 @@ const useExportOrganizationUsersMutation = (
         return options.onMutate?.(...rest)
       },
       onSuccess: (data, ...rest) => {
-        if (data.results?.length) {
-          const file = jsonToXlsxFile(data.results ?? [])
-          saveAs(file, `growthday-enterprise-users-${moment().format('DD-MM-YY-HH-mm-ss')}.xlsx`)
-        }
+        saveAs(data, `growthday-enterprise-users-${moment().format('DD-MM-YY-HH-mm-ss')}.xlsx`)
         return options.onSuccess?.(data, ...rest)
       }
     }
