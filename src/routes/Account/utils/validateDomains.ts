5eb2b1d22c6c717572a0860538d72330d38ff632
Hariom Sharma <hariom.s2510@gmail.com>
2022-05-30 16:58:51 +0530

    fix: lower case domain
    https://growthday.atlassian.net/browse/GDCV1-9441

--- src/routes/Account/utils/validateDomains.ts
+++ src/routes/Account/utils/validateDomains.ts
@@ -1,8 +1,9 @@
+import { toLower } from 'lodash-es'
 import coerceArray from '../../../utils/coerceArray'
 
 const validateDomains = (values: string | string[]): string[] =>
   (coerceArray(values) as string[])
-    .flatMap((value) => value.split(',').map((domain) => (domain ?? '').trim()))
+    .flatMap((value) => value.split(',').map((domain) => toLower(domain).trim()))
     .filter(Boolean)
 // .filter((domain) => emailDomainRegex.test(domain))
 
